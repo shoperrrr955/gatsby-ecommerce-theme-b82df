0de499c477ec570b4327cd6f9a24ed7490c93d85
shoperrrr955 <aboubakarovm@gmail.com>
2025-04-30 11:49:46 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -8,10 +8,10 @@ export default defineStackbitConfig({
     ssgVersion: '5',
     contentSources: [
         new ContentfulContentSource({
-            spaceId: process.env.CONTENTFUL_SPACE_ID!,
-            environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
-            previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!
+            spaceId: process.env.CONTENTFUL_SPACE_ID,
+            environment: process.env.CONTENTFUL_ENVIRONMENT,
+            previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN
         })
     ],
     modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
