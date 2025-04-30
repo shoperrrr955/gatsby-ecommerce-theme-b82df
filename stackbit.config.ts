9b3e9076b929c49e259aa747d54fcbf51cef8150
shoperrrr955 <aboubakarovm@gmail.com>
2025-04-30 11:27:25 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,51 +1,29 @@
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
 import { defineStackbitConfig } from '@stackbit/types';
 
 export default defineStackbitConfig({
-  stackbitVersion: '~0.5.0',
-  ssg: 'gatsby',
-  nodeVersion: '18',
-  contentSources: [
-    {
-      name: 'assets',
-      path: 'src/assets',
-      commitMessage: 'Add favicon',
-      commitDate: '3 years ago'
-    },
-    {
-      name: 'components',
-      path: 'src/components',
-      commitMessage: 'Bumping versions, using Image CDN (#134)',
-      commitDate: 'last year'
-    },
-    {
-      name: 'context',
-      path: 'src/context',
-      commitMessage: 'adds prettier and runs it on write',
-      commitDate: '3 years ago'
-    },
-    {
-      name: 'helpers',
-      path: 'src/helpers',
-      commitMessage: 'Bumping versions, using Image CDN (#134)',
-      commitDate: 'last year'
-    },
-    {
-      name: 'pages',
-      path: 'src/pages',
-      commitMessage: 'Bumping versions, using Image CDN (#134)',
-      commitDate: 'last year'
-    },
-    {
-      name: 'plop',
-      path: 'src/plop',
-      commitMessage: 'adds prettier and runs it on write',
-      commitDate: '3 years ago'
-    },
-    {
-      name: 'config.json',
-      path: 'src/config.json',
-      commitMessage: 'changes # links to /shop',
-      commitDate: '3 years ago'
+    stackbitVersion: '~0.6.0',
+    nodeVersion: '18',
+    ssgName: 'gatsby',
+    ssgVersion: '5',
+    contentSources: [
+        new ContentfulContentSource({
+            spaceId: process.env.CONTENTFUL_SPACE_ID!,
+            environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
+            previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
+            accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!
+        })
+    ],
+    modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }],
+    // Needed only for importing this repository via https://app.stackbit.com/import?mode=duplicate
+    import: {
+        type: 'contentful',
+        contentFile: 'contentful/export.json',
+        uploadAssets: true,
+        assetsDirectory: 'contentful',
+        spaceIdEnvVar: 'CONTENTFUL_SPACE_ID',
+        deliveryTokenEnvVar: 'CONTENTFUL_DELIVERY_TOKEN',
+        previewTokenEnvVar: 'CONTENTFUL_PREVIEW_TOKEN',
+        accessTokenEnvVar: 'CONTENTFUL_MANAGEMENT_TOKEN'
     }
-  ]
 });
